ab2bad3fd7c80b4d94031e72b76ed38377ceb81a
davidlei <davidleitw@gmail.com>
2019-09-05 18:51:11 +0800

    unit test function

--- data_preparation/get_density_map_gayssian.py
+++ data_preparation/get_density_map_gayssian.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def Get_Density_Map_Gaussian(Image, points):
     Image_density = np.zeros((Image.shape), dtype=int)
-    H, W = Image_density.shape
-
+    H, W, C = Image_density.shape
+    
     if len(points) == 0:
         break
 
